8762a1ff3042dec74cb509de9f6f399a77db8341
Garvit Rastogi <garvitrastogi2055@gmail.com>
2026-06-17 09:50:20 +0530

    Add Express Router structure

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 const express = require("express");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
-
-app.get("/health", (req, res) => {
-  res.send("Server Healthy");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("Contact Page");
-});
-
-app.get("/profile", (req, res) => {
-  res.send("Profile Page");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
-  console.log("Server running on port 5000");
+    console.log("Server running on port 5000");
 });
