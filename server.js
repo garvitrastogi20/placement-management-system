fcf853acf9c122f89a0395223036ca872c98030e
Garvit Rastogi <garvitrastogi2055@gmail.com>
2026-06-17 10:09:57 +0530

    Add JSON middleware

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
 const authRoutes = require("./routes/authRoutes");
+const logger = require("./middleware/loggerMiddleware");
 
 const app = express();
+app.use(express.json());
+
+app.use(logger);
 
 app.use("/api/auth", authRoutes);
 
